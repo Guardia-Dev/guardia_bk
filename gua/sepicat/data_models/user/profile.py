7e4600bd8299d1bd066bdecfd0422d0320d85b88
Harry Twan <gua@desgard.com>
2019-01-31 21:56:34 +0800

    更新 state 获取
    Signed-off-by: Harry Twan <gua@desgard.com>

--- gua/sepicat/data_models/user/profile.py
+++ gua/sepicat/data_models/user/profile.py
@@ -16,9 +16,28 @@ def fetch_state(login: str, header: dict = {}) -> dict:
         resp = requests.get(url, header=header)
     html_str = resp.content
     # print(html_str)
+    res = {}
     soup = BeautifulSoup(html_str, 'lxml')
 
+    while True:
+        # 先判断是否有 GitHub 自定义 emoji
+        emoji_element = soup.find("img", attrs={
+            "class": "emoji",
+        })
+        if emoji_element is not None:
+            icon = emoji_element.attrs['src']
+            title = emoji_element.attrs['title']
+
+    state_title = soup.find("div", re.compile(r'^[\s\S]*?user-status-message-wrapper.+?$'))
+    res['state'] = {
+        'icon': icon,
+        'title': title,
+        'text': state_title.findChild().text,
+    }
+
+    return res
 
 if __name__ == "__main__":
     login = "Desgard"
+    login = "halfrost"
     print(fetch_state(login=login))
